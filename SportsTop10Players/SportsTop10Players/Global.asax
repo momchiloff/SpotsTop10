6d6a5b92640f71b148fda4222bb319238d84381e
momchiloff <velinovmomchilov@gmail.com>
2016-05-11 01:46:57 +0300

    Sports Top 10 Players

---
+++ SportsTop10Players/SportsTop10Players/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportsTop10Players.MvcApplication" Language="C#" %>
---
+++ SportsTop10Players/SportsTop10Players/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportsTop10Players.Startup))]
+namespace SportsTop10Players
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
